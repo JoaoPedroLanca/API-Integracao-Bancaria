d03977bb3efe09a9b630e06b0006f6a336ef864a
João Pedro Lança <joaolanca53@gmail.com>
2025-09-05 18:24:44 -0300

    init: first commit

---
+++ src/main/java/com/apibancaria/APIIntegracaoBancaria/ApiIntegracaoBancariaApplication.java
@@ -0,0 +1,13 @@
+package com.apibancaria.APIIntegracaoBancaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiIntegracaoBancariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiIntegracaoBancariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apibancaria/APIIntegracaoBancaria/ApiIntegracaoBancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apibancaria.APIIntegracaoBancaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiIntegracaoBancariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
