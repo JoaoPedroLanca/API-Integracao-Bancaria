a465e593acdb1fb78c740157d39041f4e3f7ea9e
João Pedro Lança <joaolanca53@gmail.com>
2025-09-08 16:28:53 -0300

    feat: Add Spring Security credentials in UserModel

--- src/main/java/com/apibancaria/APIIntegracaoBancaria/Enums/Role.java
+++ src/main/java/com/apibancaria/APIIntegracaoBancaria/Enums/Role.java
@@ -1,6 +1,13 @@
 package com.apibancaria.APIIntegracaoBancaria.Enums;
 
-public enum Role {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
     USUARIO,
     ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name() ;
+    }
 }
---
+++ src/main/java/com/apibancaria/APIIntegracaoBancaria/Model/UserModel.java
@@ -0,0 +1,73 @@
+package com.apibancaria.APIIntegracaoBancaria.Model;
+
+import com.apibancaria.APIIntegracaoBancaria.Enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class UserModel implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    String nome;
+
+    String email;
+
+    String senha;
+
+    @Enumerated(EnumType.STRING)
+    Role role;
+
+    LocalDate createdAt;
+
+    LocalDate updatedAt;
+
+    @Override
+    public String getUsername(){
+        return getEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        return getSenha();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(role);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
