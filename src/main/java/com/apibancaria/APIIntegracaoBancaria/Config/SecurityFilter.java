60422a8d3fae67a6d34383d36acad061f057b554
João Pedro Lança <joaolanca53@gmail.com>
2025-09-26 10:58:53 -0300

    feat: Create a filter that intercepts requests and checks if the token is valid.

---
+++ src/main/java/com/apibancaria/APIIntegracaoBancaria/Config/SecurityFilter.java
@@ -0,0 +1,59 @@
+package com.apibancaria.APIIntegracaoBancaria.Config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+
+    public SecurityFilter(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String authorizatinoHeader = request.getHeader("Authorization");
+
+        if (Strings.isNotEmpty(authorizatinoHeader) && authorizatinoHeader.startsWith("Bearer ")) {
+            String token = authorizatinoHeader.substring("Bearer ".length());
+
+            Optional<JWTUserData> optJwtUserData = tokenService.verifyToken(token);
+
+            if (optJwtUserData.isPresent()){
+                JWTUserData userData = optJwtUserData.get();
+
+                List<GrantedAuthority> authorities = List.of(
+                        new SimpleGrantedAuthority("ROLE_" + userData.role().name())
+                );
+
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userData, null, authorities);
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+
+            filterChain.doFilter(request, response);
+        } else {
+            filterChain.doFilter(request, response);
+        }
+
+    }
+}
